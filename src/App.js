207d7c738e913239721683aa1c1c5000f02e08fe
Alex Travagin Chatalov <alex_tr1992@hotmail.com>
2019-08-07 20:47:33 -0300

    adicionado a tabela para lista, alterado a cor do menu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import "./style.css";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from "./home/Home"
 import Renda from "./rendas/Renda";
--- src/rendas/Renda.js
+++ src/rendas/Renda.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import {withStyles} from '@material-ui/styles';
+import { withStyles } from '@material-ui/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const StyledTableCell = withStyles(theme => ({
+  head: {
+    backgroundColor: '#009688',
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles(theme => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.background.default,
+    },
+  },
+}))(TableRow);
 
 const styles = theme => ({
   container: {
@@ -19,6 +43,11 @@ const styles = theme => ({
   menu: {
     width: 200,
   },
+  root: {
+    width: '20%',
+    marginTop: theme.spacing(3),
+    overflowX: 'auto',
+  },
 });
 
 
@@ -35,7 +64,7 @@ class Renda extends Component {
   }
   onChange = (event) => {
     event.preventDefault();
-    const state = Object.assign({},this.state);
+    const state = Object.assign({}, this.state);
     state[event.target.name] = event.target.value;
     this.setState(state);
   }
@@ -45,7 +74,7 @@ class Renda extends Component {
     event.persist();
     console.log(event);
     const rendasList = this.state.rendas.slice();
-    rendasList.push({renda:this.state.renda, valor:this.state.valor});
+    rendasList.push({ renda: this.state.renda, valor: this.state.valor });
     console.log(rendasList);
     this.setState({
       renda: '',
@@ -55,37 +84,57 @@ class Renda extends Component {
   }
 
   render() {
-    const {classes: classe} = this.props;
+    const { classes: classe } = this.props;
     return (
       <div>
         <form className={classe} onSubmit={this.onSubmit}>
-            <TextField
-                required
-                id="rend"
-                label="Renda"
-                name="renda"
-                className={classe.textField}
-                margin="normal"
-                value={this.state.renda}
-                onChange={this.onChange}
-            />
-               <TextField
-                required
-                id="valor"
-                label="Valor"
-                name="valor"
-                className={classe.textField}
-                margin="normal"
-                value={this.state.valor}
-                onChange={this.onChange}
-            />
+          <TextField
+            required
+            id="rend"
+            label="Renda"
+            name="renda"
+            className={classe.textField}
+            margin="normal"
+            value={this.state.renda}
+            onChange={this.onChange}
+          />
+          <TextField
+            required
+            id="valor"
+            label="Valor"
+            name="valor"
+            className={classe.textField}
+            margin="normal"
+            value={this.state.valor}
+            onChange={this.onChange}
+          />
+          <br/>
           <Button variant="contained" color="primary" type="submit">
             Adicionar
           </Button>
         </form>
-        {
-        this.state.rendas.map((renda,i) =>  <div key={i}>{renda.renda}: {renda.valor}</div>)
-        }
+        <div align="center">
+          <Paper className={classe.root}>
+            <Table >
+              <TableHead>
+                <TableRow>
+                  <StyledTableCell align="center">Renda</StyledTableCell>
+                  <StyledTableCell align="left">Valor</StyledTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {this.state.rendas.map((renda, i) => (
+                  <StyledTableRow key={i}>
+                    <StyledTableCell component="th" scope="row">
+                      {renda.renda}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">{renda.valor}</StyledTableCell>
+                  </StyledTableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </Paper>
+        </div>
       </div>
       /*
       <RendaView
